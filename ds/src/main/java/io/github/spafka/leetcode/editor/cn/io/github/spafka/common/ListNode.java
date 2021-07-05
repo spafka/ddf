f264fde85bbe911e793091ee858330381142df6e
spafka <spafka@163.com>
2021-07-06 00:51:33 +0800

    add merge sort

---
+++ ds/src/main/java/io/github/spafka/leetcode/editor/cn/io/github/spafka/common/ListNode.java
@@ -0,0 +1,22 @@
+package io.github.spafka.leetcode.editor.cn.io.github.spafka.common;
+
+import lombok.Data;
+
+@Data
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
